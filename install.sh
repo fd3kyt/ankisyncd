42ec5ef835a034775fc3a8158f85b16ec79239e8
fd3kyt <fd3kyt@gmail.com>
2018-10-26 11:20:57 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,8 @@ apt-get install portaudio19-dev
 apt-get install python3
 apt-get install python3-pip
 python3 -m pip install --upgrade pip
-python3 -m pip install webob
 
 export LC_ALL=C                 # for pip
-
 git submodule update --init
-cd anki-bundled || exit 1
-python3 -m pip install -r requirements.txt
+(cd anki-bundled && python3 -m pip install -r requirements.txt && make install)
+python3 -m pip install .
